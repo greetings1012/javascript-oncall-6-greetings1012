08de0ee5d42a5011ea745d91d0370d44a7a612c2
Younghoon Ahn <ahnqwerty@gmail.com>
2023-12-16 18:00:12 +0900

    fix: 근무표 계산 코드 수정 완료, 근무표 출력 코드 구현

--- src/EmergencyScheduleGenerator.js
+++ src/EmergencyScheduleGenerator.js
@@ -18,19 +18,39 @@ const EmergencyScheduleGenerator = {
 
     getScheduleSet(month, totalDays, dayOfWeekInNumber, weekDaySchedule, offDaySchedule) {
         const scheduleSet = new Set();
-
         let totalWeekdaySchedule = Array.from({ length: totalDays }, (_, index) => weekDaySchedule[index % weekDaySchedule.length]);
         let totalOffdaySchedule = Array.from({ length: totalDays }, (_, index) => offDaySchedule[index % offDaySchedule.length]);
-
+        let lastWorker = ''
         for (let day = 1; day <= totalDays; day++) {
             const currentDayOfWeek = (day + dayOfWeekInNumber - 1) % 7; // 요일 계산
-            const isHoliday = NATIONAL_HOLIDAY[month] && NATIONAL_HOLIDAY[month] === day;
+            const isHoliday = ((NATIONAL_HOLIDAY[month] && NATIONAL_HOLIDAY[month] === day) || (currentDayOfWeek == 6 || currentDayOfWeek == 0));
+            
+            let currentWorker;
 
             if (isHoliday) {
-                scheduleSet.add({ day, dayOfWeek: this.getKoreanDayOfWeek(currentDayOfWeek), name: totalOffdaySchedule.shift() });
-            } else {
-                scheduleSet.add({ day, dayOfWeek: this.getKoreanDayOfWeek(currentDayOfWeek), name: totalWeekdaySchedule.shift() });
+                currentWorker = totalOffdaySchedule.shift();
+
+                // 휴일 다음 평일 근무가 연속될 경우 순서를 바꿈
+                if (lastWorker && lastWorker === currentWorker) {
+                    const nextWorker = totalOffdaySchedule.shift();
+                    totalOffdaySchedule.unshift(currentWorker);
+                    currentWorker = nextWorker;
+                }
+            } 
+            else {
+                currentWorker = totalWeekdaySchedule.shift();
+
+                // 평일 다음 휴일 근무가 연속될 경우 순서를 바꿈
+                if (lastWorker && lastWorker === currentWorker) {
+                    const nextWorker = totalWeekdaySchedule.shift();
+                    totalWeekdaySchedule.unshift(currentWorker);
+                    currentWorker = nextWorker;
+                }
             }
+
+
+            scheduleSet.add({ day, dayOfWeek: this.getKoreanDayOfWeek(currentDayOfWeek), name: currentWorker });
+            lastWorker = currentWorker;
         }
 
         return scheduleSet;
